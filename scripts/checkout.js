7d1f5b79161aa05afb2a484ecb800bb46f9640f0
Yash Deswal <175260269+yash-deswal@users.noreply.github.com>
2026-05-01 02:34:17 +0530

    Add promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,21 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
 import {loadProducts} from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
-loadProducts(() => {
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  });
+}).then(() => {
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+}).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
