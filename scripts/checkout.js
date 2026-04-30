44eecb358b20db2d9c9d6a7210e5ea7cc932283d
Yash Deswal <175260269+yash-deswal@users.noreply.github.com>
2026-05-01 02:53:07 +0530

    Convert loadProducts from xhr to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,23 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
-import {loadProducts} from "../data/products.js";
+import {loadProducts, loadProductsFetch} from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
+Promise.all([
+  loadProductsFetch(),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+]).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
+/*
 new Promise((resolve) => {
   loadProducts(() => {
     resolve();
@@ -19,3 +32,4 @@ new Promise((resolve) => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
